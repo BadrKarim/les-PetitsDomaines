f3cf7f88fb302368dbe48807c62ab1d6ca0d249f
BadrKarim <karim-badr@hotmail.fr>
2024-01-26 11:57:34 +0100

    hasher password

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -21,7 +22,7 @@ class RegisterController extends AbstractController
     }
 
     #[Route('/inscription', name: 'app_register')]
-    public function index(Request $request): Response
+    public function index(Request $request, UserPasswordHasherInterface $hasher): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
@@ -30,10 +31,12 @@ class RegisterController extends AbstractController
 
         if($form->isSubmitted() && $form->isValid()){
             $user = $form->getData();
+            $password = $hasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($password);
+            //dd($password);
 
             $this->entityManager->persist($user);
             $this->entityManager->flush();
-            // dd($user);
         }
 
         return $this->render('register/index.html.twig', [
