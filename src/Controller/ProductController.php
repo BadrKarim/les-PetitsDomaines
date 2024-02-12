db06bb72b9bde4264c1d54aa1ae2dd8ff3fd880d
BadrKarim <karim-badr@hotmail.fr>
2024-02-12 14:37:10 +0100

    Filtre et barre de recherche

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Classes\Search;
 use App\Entity\Product;
+use App\Form\SearchType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,12 +21,24 @@ class ProductController extends AbstractController
     }
 
     #[Route('/nos-produits', name: 'app_products')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
+
+        $search = new Search();
+        $formSearch = $this->createForm(SearchType::class, $search);
+
+        $formSearch->handleRequest($request);
+        if($formSearch->isSubmitted() && $formSearch->isValid())
+        {
+            //dd($search);
+            $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        }else {
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
+        }
 
         return $this->render('product/index.html.twig', [
-            'products' => $products
+            'products' => $products,
+            'formSearch' => $formSearch->createView()
         ]);
     }
     
