6264d616846208755198b3295bba0bfc88084900
BadrKarim <karim-badr@hotmail.fr>
2024-02-13 16:12:19 +0100

    Ajouter enlever un produit et vider le panier

--- src/Classes/Cart.php
+++ src/Classes/Cart.php
@@ -1,15 +1,19 @@
 <?php
 namespace App\Classes;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 class Cart
 {
     private $requestStack;
+    private $entityManager;
 
-    public function __construct(RequestStack $requestStack)
+    public function __construct(RequestStack $requestStack, EntityManagerInterface $entityManager)
     {
         $this->requestStack = $requestStack;
+        $this->entityManager = $entityManager;
     }
 
     public function add($id)
@@ -23,18 +27,6 @@ class Cart
             // Sinon, ajoute le produit au panier avec une quantité de 1
             $cart[$id] = 1;
         }
-
-        // $cart = $session->get('cart', [
-        //     'id' => $id,
-        //     'quantity' => 1
-        // ]);
-
-        // if(!empty($cart[$id]))
-        // {
-        //     $cart[$id]++;
-        // }else {
-        //     $cart[$id] = 1;
-        // }
             
         $session->set('cart', $cart);
         
@@ -43,6 +35,8 @@ class Cart
 
     public function get()
     {
+        //dd($cart->get());
+        // Assurez-vous que la méthode get() de la classe Cart retourne toujours un tableau valide
         $session = $this->requestStack->getSession();
         return $session->get('cart', []);
     }
@@ -52,5 +46,67 @@ class Cart
         $session = $this->requestStack->getSession();
         return $session->remove('cart');
     }
+
+    public function delete($id)
+    {
+        $session = $this->requestStack->getSession();
+    $cart = $session->get('cart', []);
+
+    // Vérifier si le produit existe dans le panier
+    if (isset($cart[$id])) {
+        // Supprimer le produit du panier
+        unset($cart[$id]);
+        // Mettre à jour le panier en session
+        $session->set('cart', $cart);
+    }
+
+    return $cart;
+    }
+
+    public function decrease($id)
+    {
+        $session = $this->requestStack->getSession();
+        $cart = $session->get('cart', []);
+
+        //vérifier si la quantité du produit est suppérieur à 1 et cart n'est pas null
+        if (isset($cart[$id]) && $cart[$id] > 1) {
+            $cart[$id]--;
+            $session->set('cart', $cart);
+
+        //Si la quantité du produit est strictement égale à 1 et cart n'est pas null supprimer le produit du panier
+        } elseif (isset($cart[$id]) && $cart[$id] === 1) {
+            unset($cart[$id]);
+            $session->set('cart', $cart);
+        }
+
+        return $cart;
+    }
+
+    public function getFull()
+    {
+        $cartItems = $this->get();
+        $cartComplete = [];
+    
+        // Vérifiez si le panier n'est pas vide
+        if ($cartItems) {
+            foreach ($cartItems as $id => $quantity) {
+                // Recherchez le produit correspondant à l'ID dans la base de données
+                $product = $this->entityManager->getRepository(Product::class)->find($id);
+                if ($product) {
+                    $cartComplete[] = [
+                        'product' => $product,
+                        'quantity' => $quantity
+                    ];
+                } else {
+                    // Si le produit n'existe pas, supprimez-le du panier
+                    $this->delete($id);
+                }
+            }
+        }
+    
+        return $cartComplete;
+    }
+    
+
 }
 
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,49 +3,20 @@
 namespace App\Controller;
 
 use App\Classes\Cart;
-use App\Entity\Product;
-use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
-    private $entityManager;
 
-    public function __construct(EntityManagerInterface $entityManager)
-    {
-        $this->entityManager = $entityManager;
-    }
 
     #[Route('/mon-panier', name: 'app_cart')]
     public function index(Cart $cart): Response
     {
-        //dd($cart->get());
-        // Assurez-vous que la méthode get() de la classe Cart retourne toujours un tableau valide
-        $cartItems = $cart->get();
-        if ($cartItems === null) {
-            $cartItems = [];
-        }
-
-        $cartComplete = [];
-        foreach($cartItems as $id => $quantity){
-            // Vérifiez si l'identifiant est valide
-            //dd($id)
-            $product = $this->entityManager->getRepository(Product::class)->findOneById($id);
-            if ($product !== null) {
-                $cartComplete[] = [
-                    'product' => $product,
-                    'quantity' => $quantity
-                ];
-            }
-            // $cartComplete[] = [
-            //     'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
-            //     'quantity' => $quantity
-            // ];
-        }
-        //dd($cartComplete);
-
+        // Récupérer les éléments complets du panier
+        $cartComplete = $cart->getFull();
         return $this->render('cart/index.html.twig', [
             'cart' => $cartComplete //$cart->get()
         ]);
@@ -69,5 +40,23 @@ class CartController extends AbstractController
         $cart->remove();
 
         return $this->redirectToRoute('app_products');
+    }
+
+    #[Route('/cart/delete/{id}', name: 'app_delete_to_cart')]
+    public function delete(Cart $cart, $id): Response
+    {
+        // Retirez tous les produits du panier
+        $cart->delete($id);
+
+        return $this->redirectToRoute('app_cart');
+    }
+
+    #[Route('/cart/decrease/{id}', name: 'app_decrease_to_cart')]
+    public function decrease(Cart $cart, $id): Response
+    {
+        
+        $cart->decrease($id);
+
+        return $this->redirectToRoute('app_cart');
     }
 }
