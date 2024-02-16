91dfd47cae9b373546e48e1e47667d2bb2bddec8
BadrKarim <karim-badr@hotmail.fr>
2024-02-16 15:39:21 +0100

    Tunnel d'achat commande, Valider la commande

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -5,13 +5,16 @@ namespace App\Controller;
 use App\Classes\Cart;
 use App\Form\OrderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
+
+
     #[Route('/commande', name: 'app_order')]
-    public function index(Cart $cart): Response
+    public function index(Cart $cart, Request $request): Response
     {
         //dd($this->getUser()->getAddresses()->getValues());
         // si user n'a pas d'adresse redirige vers aad address
@@ -24,6 +27,13 @@ class OrderController extends AbstractController
             // pour que mon form n'envoie que les adresse liés à un user
             'user' => $this->getUser()
         ]);
+
+        $formOrder->handleRequest($request);
+
+        if ($formOrder->isSubmitted() && $formOrder->isValid()) {
+            dd($formOrder->getData());
+        }
+
         return $this->render('order/index.html.twig', [
             'formOrder' => $formOrder->createView(),
             'cart' => $cart->getFull()
