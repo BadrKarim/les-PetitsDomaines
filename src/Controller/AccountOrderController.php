dcf230519cdd3af817a55c9ec8df51b35f86febc
BadrKarim <karim-badr@hotmail.fr>
2024-03-04 12:53:55 +0100

    MVC order coté client

--- src/Controller/AccountOrderController.php
+++ src/Controller/AccountOrderController.php
@@ -8,11 +8,9 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class AccountOrderController extends AbstractController
 {
-    #[Route('/account/order', name: 'app_account_order')]
+    #[Route('/compte/mes-commandes', name: 'account_order')]
     public function index(): Response
     {
-        return $this->render('account_order/index.html.twig', [
-            'controller_name' => 'AccountOrderController',
-        ]);
+        return $this->render('account/order.html.twig');
     }
 }
