abca26f302e5a8f6e5373c64bd1116767e84a5ad
BadrKarim <karim-badr@hotmail.fr>
2024-03-08 16:17:58 +0100

    Refactor CartController & CartClass

--- src/Classes/Cart.php
+++ src/Classes/Cart.php
@@ -9,76 +9,18 @@ class Cart
 {
     private $requestStack;
     private $entityManager;
-
+    
     public function __construct(RequestStack $requestStack, EntityManagerInterface $entityManager)
     {
         $this->requestStack = $requestStack;
         $this->entityManager = $entityManager;
     }
 
-    public function add($id)
-    {
-        $session = $this->requestStack->getSession();
-        $cart = $session->get('cart', []);
-
-        if (isset($cart[$id])) {
-            //Si le produit existe déjà dans le panier, incrémente la quantité
-            $cart[$id]++;
-        }else {
-            // Sinon, ajoute le produit au panier avec une quantité de 1
-            $cart[$id] = 1;
-        }
-
-        $session->set('cart', $cart);
-    }
-
     public function get()
     {
-        //dd($cart->get());
         $session = $this->requestStack->getSession();
 
         return $session->get('cart', []);
-    }
-
-    public function remove()
-    {
-        $session = $this->requestStack->getSession();
-        return $session->remove('cart');
-    }
-
-    public function delete($id)
-    {
-        $session = $this->requestStack->getSession();
-        $cart = $session->get('cart', []);
-
-        // Vérifier si le produit existe dans le panier
-        if (isset($cart[$id])) {
-            // Supprimer le produit du panier
-            unset($cart[$id]);
-            // Mettre à jour le panier en session
-            $session->set('cart', $cart);
-        }
-
-        return $cart;
-    }
-
-    public function decrease($id)
-    {
-        $session = $this->requestStack->getSession();
-        $cart = $session->get('cart', []);
-
-        //vérifier si la quantité du produit est suppérieur à 1 et cart n'est pas null
-        if (isset($cart[$id]) && $cart[$id] > 1) {
-            $cart[$id]--;
-            $session->set('cart', $cart);
-
-        //Si la quantité du produit est strictement égale à 1 et cart n'est pas null supprimer le produit du panier
-        } elseif (isset($cart[$id]) && $cart[$id] === 1) {
-            unset($cart[$id]);
-            $session->set('cart', $cart);
-        }
-
-        return $cart;
     }
 
     public function getFull()
@@ -106,6 +48,63 @@ class Cart
         }
 
         return $cartComplete;
+    }
+
+    public function add($id)
+    {
+        $session = $this->requestStack->getSession();
+        $cart = $session->get('cart', []);
+
+        if (isset($cart[$id])) {
+            //Si le produit existe déjà dans le panier, incrémente la quantité
+            $cart[$id]++;
+        }else {
+            // Sinon, ajoute le produit au panier avec une quantité de 1
+            $cart[$id] = 1;
+        }
+
+        $session->set('cart', $cart);
+    }
+
+    public function decrease($id)
+    {
+        $session = $this->requestStack->getSession();
+        $cart = $session->get('cart', []);
+
+        //vérifier si la quantité du produit est suppérieur à 1 et cart n'est pas null
+        if (isset($cart[$id]) && $cart[$id] > 1) {
+            $cart[$id]--;
+            $session->set('cart', $cart);
+
+        //Si la quantité du produit est strictement égale à 1 et cart n'est pas null supprimer le produit du panier
+        } elseif (isset($cart[$id]) && $cart[$id] === 1) {
+            unset($cart[$id]);
+            $session->set('cart', $cart);
+        }
+
+        return $cart;
+    }
+
+    public function delete($id)
+    {
+        $session = $this->requestStack->getSession();
+        $cart = $session->get('cart', []);
+
+        // Vérifier si le produit existe dans le panier
+        if (isset($cart[$id])) {
+            // Supprimer le produit du panier
+            unset($cart[$id]);
+            // Mettre à jour le panier en session
+            $session->set('cart', $cart);
+        }
+
+        return $cart;
+    }
+
+    public function remove()
+    {
+        $session = $this->requestStack->getSession();
+        return $session->remove('cart');
     }
 }
 
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Classes\Cart;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,7 +14,6 @@ class CartController extends AbstractController
     {
         // Récupérer les éléments complets du panier
         $cartComplete = $cart->getFull();
-        //$cart->get()
 
         return $this->render('cart/index.html.twig', [
             'cart' => $cartComplete 
@@ -25,27 +23,8 @@ class CartController extends AbstractController
     #[Route('/mon-panier/add/{id}', name: 'add_cart')]
     public function add(Cart $cart, $id): Response
     {
-        //dd($id);
         // Ajouter un produit au panier
         $cart->add($id);
-
-        return $this->redirectToRoute('cart');
-    }
-
-    #[Route('/mon-panier/remove', name: 'remove_cart')]
-    public function remove(Cart $cart): Response
-    {
-        // Retirer tous les produits du panier
-        $cart->remove();
-
-        return $this->redirectToRoute('products');
-    }
-
-    #[Route('/mon-panier/delete/{id}', name: 'delete_cart')]
-    public function delete(Cart $cart, $id): Response
-    {
-        // Retirer un produit du panier
-        $cart->delete($id);
 
         return $this->redirectToRoute('cart');
     }
@@ -57,5 +36,23 @@ class CartController extends AbstractController
         $cart->decrease($id);
 
         return $this->redirectToRoute('cart');
+    }
+
+    #[Route('/mon-panier/delete/{id}', name: 'delete_cart')]
+    public function delete(Cart $cart, $id): Response
+    {
+        // Retirer un produit du panier
+        $cart->delete($id);
+
+        return $this->redirectToRoute('cart');
+    }
+
+    #[Route('/mon-panier/remove', name: 'remove_cart')]
+    public function remove(Cart $cart): Response
+    {
+        // Retirer tous les produits du panier
+        $cart->remove();
+
+        return $this->redirectToRoute('products');
     }
 }
